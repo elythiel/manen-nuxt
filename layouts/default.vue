43bed9886c7370a78d072038726e9b110882fa72
Mickaël 'Martelle' Maillot <mickael.o.maillot@gmail.com>
2021-01-25 19:13:12 +0100

    Added index page

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,14 @@
   <div>
     <Header />
 
-    <Nuxt />
+    <div class="container mx-auto px-24 py-12 text-2xl text-primary">
+      <Nuxt />
+    </div>
   </div>
 </template>
+
+<style lang="postcss">
+body {
+  @apply bg-secondary-light font-serif;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,52 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        manen-nuxt
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="text-justify">
+    <img
+      src="~/assets/band.png"
+      alt="Photo des membres de Manen"
+      class="mb-12"
+    />
+    <div class="px-8 my-12">
+      <p class="mb-2">
+        Le groupe Månen est constitué de deux frères à la composition et d'un
+        ami à l'écriture.
+      </p>
+      <p class="mb-2">
+        Månen existe depuis une dizaine d'années, ils se produisent sur de
+        petites scènes en région parisienne.
+      </p>
+      <p class="mb-2">
+        Le nom Månen signifiant "la lune" en norvégien est un clin d'œil aux
+        inspirations scandinaves et celtiques du groupe.
+      </p>
+      <p class="mb-2">
+        Månen vient récréer son propre folklore s'inspirant des mythologies et
+        légendes européennes.
+      </p>
+      <p class="mb-2">
+        La nature et les astres sont tant de muses pour leur univers épique et
+        fantaisiste.
+      </p>
+      <p>
+        En 2020 Månen sort son premier album Étendues Perdues dont la
+        composition aux accords tranchés et à la voix puissante nous emporte
+        dans une épopée aventureuse de style néo folk rock.
+      </p>
     </div>
+
+    <iframe
+      class="mx-auto"
+      width="560"
+      height="315"
+      frameborder="0"
+      src="https://www.youtube.com/embed/hYsNfLw-sgo"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
@@ -42,16 +60,8 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
