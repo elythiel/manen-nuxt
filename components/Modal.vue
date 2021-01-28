e312eff1cf08ad022b445d5909f5701517ce1f8a
Mickaël 'Martelle' Maillot <mickael.o.maillot@gmail.com>
2021-01-28 22:20:49 +0100

    Add a portal for modals in default layout

--- components/Modal.vue
+++ components/Modal.vue
@@ -3,28 +3,31 @@
     <a href="#" @click="active = true">
       <slot name="button"></slot>
     </a>
-    <div
-      v-if="active"
-      class="modal-wrapper fixed z-10 h-screen w-screen top-0 left-0 bg-black bg-opacity-25 flex items-center justify-center"
-      @click="close"
-    >
+
+    <portal to="modals">
       <div
-        class="relative flex flex-col"
-        :class="{ 'bg-white p-12 shadow-lg w-1/2 max-h-5/6': frame }"
+        v-if="active"
+        class="modal-wrapper fixed z-10 h-screen w-screen top-0 left-0 bg-black bg-opacity-25 flex items-center justify-center"
+        @click="close"
       >
-        <button
-          v-if="frame"
-          class="absolute top-0 right-0 p-4"
-          @click="active = false"
+        <div
+          class="relative flex flex-col w-5/6 md:w-1/2 max-h-5/6"
+          :class="{ 'bg-white p-12 shadow-lg text-secondary-dark': frame }"
         >
-          <close-icon />
-        </button>
-        <h2 v-if="title" class="text-3xl mb-10">{{ title }}</h2>
-        <div class="flex-grow overflow-y-auto text-lg">
-          <slot></slot>
+          <button
+            v-if="frame"
+            class="absolute top-0 right-0 p-4"
+            @click="active = false"
+          >
+            <close-icon />
+          </button>
+          <h2 v-if="title" class="text-3xl mb-10">{{ title }}</h2>
+          <div class="flex-grow overflow-y-auto text-lg">
+            <slot></slot>
+          </div>
         </div>
       </div>
-    </div>
+    </portal>
   </div>
 </template>
 
