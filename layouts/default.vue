4302214147ed9f510436ebeff5cca7fbf1390547
Mickaël 'Martelle' Maillot <mickael.o.maillot@gmail.com>
2021-04-06 19:33:47 +0200

    Fixed container and footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,11 +3,11 @@
     <div class="min-h-screen flex flex-col justify-start">
       <Header />
 
-      <div class="container text-primary mx-auto px-4 py-4 md:py-12 xl:px-0">
+      <div class="container text-primary mx-auto px-4 py-12 xl:px-0">
         <Nuxt />
       </div>
 
-      <div class="grid grid-cols-1 lg:grid-cols-3 mt-auto bg-primary py-2 px-8 text-white gap-4 flex-col">
+      <div class="grid grid-cols-1 items-center mt-auto bg-primary py-2 px-8 text-white gap-4 flex-col lg:grid-cols-3">
         <span class="text-center lg:text-left">2021 © Manen. Tous droits réservés.</span>
         <SocialLinks class="justify-center" />
         <span class="text-center lg:text-right">
