81edf9215ab41a826858f0838f360bf93a6c3b34
Mickaël 'Martelle' Maillot <mickael.o.maillot@gmail.com>
2021-04-06 18:49:43 +0200

    Bugfixes
    - Prevent event defaults action when opening a modal
    - Added a placeholder file in concerts content folder
    - Fixed footer elements alignment on mobile & desktop

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,12 +7,14 @@
         <Nuxt />
       </div>
 
-      <div class="flex mt-auto items-center justify-between bg-primary py-2 px-8 text-white space-x-4 flex-col lg:flex-row">
-        <span class="justify-self-start">2021 © Manen. Tous droits réservés.</span>
-        <SocialLinks />
-        <a class="text-3xl hover:underline" href="https://github.com/elythiel/manen-nuxt" target="_blank">
-          <github-icon />
-        </a>
+      <div class="grid grid-cols-1 lg:grid-cols-3 mt-auto bg-primary py-2 px-8 text-white gap-4 flex-col">
+        <span class="text-center lg:text-left">2021 © Manen. Tous droits réservés.</span>
+        <SocialLinks class="justify-center" />
+        <span class="text-center lg:text-right">
+          <a class="text-3xl hover:underline" href="https://github.com/elythiel/manen-nuxt" target="_blank">
+            <github-icon />
+          </a>
+        </span>
       </div>
     </div>
 
