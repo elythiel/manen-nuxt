e4ac0e670eaf9e5e7cd263d9fce097d8ee75af92
Mickaël 'Martelle' Maillot <mickael.o.maillot@gmail.com>
2021-05-05 18:42:48 +0200

    Fixed open graph
    - Moved og image to static folder
    - Changed og image config in nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,12 +20,12 @@ export default {
         name: 'description',
         content: 'Le groupe Månen est constitué de deux frères à la composition et d\'un ami à l\'écriture. Månen existe depuis une dizaine d\'années, ils se produisent sur de petites scènes en région parisienne.'
       },
-      { hid: 'og:title', name: 'og:title', content: 'Manen' },{ hid: 'og:image', name: 'og:image', content: process.env.BASE_URL + '/images/logo-sword-1200.jpg'},
+      { hid: 'og:title', name: 'og:title', content: 'Manen' },
       { hid: 'twitter:title', name: 'twitter:title', content: 'Manen' },
       { hid: 'og:description', name: 'og:description', content: 'Le groupe Månen existe depuis une dizaine d\'années et se produisent sur de petites scènes en région parisienne.' },
       { hid: 'twitter:description', name: 'twitter:description', content: 'Le groupe Månen existe depuis une dizaine d\'années et se produisent sur de petites scènes en région parisienne.' },
-      { hid: 'og:image', name: 'og:image', content: '/images/logo-sword-1200.jpg'},
-      { hid: 'twitter:image', name: 'og:image', content: '/images/logo-sword-1200.jpg'},
+      { hid: 'og:image', name: 'og:image', content: '/images/og-image.jpg'},
+      { hid: 'twitter:image', name: 'twitter:image', content: '/images/og-image.jpg'},
       { property: "og:image:width", content: "1200" },
       { property: "og:image:height", content: "1200" }
     ],
